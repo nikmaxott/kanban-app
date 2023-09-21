0910b0739e07d9c05963eb826d756b7ed3df0cea
Nikolaus-Maxim Zolnhofer <nikolaus14@k-c13.org>
2023-09-21 10:44:06 +0100

    initial user login

---
+++ src/components/Account.tsx
@@ -0,0 +1,107 @@
+import { FormEvent, useEffect, useState } from "react";
+import { supabase } from "../helpers/supabase";
+import { Session } from "@supabase/supabase-js"
+
+export default function Account({ session } : { session: Session}): JSX.Element {
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState<string>();
+  const [website, setWebsite] = useState<string>();
+  const [avatar_url, setAvatarUrl] = useState<string>();
+
+  useEffect(() => {
+    async function getProfile() {
+      setLoading(true);
+      const { user } = session;
+
+      let { data, error } = await supabase
+        .from("profiles")
+        .select(`username, website, avatar_url`)
+        .eq("id", user.id)
+        .single();
+
+      if (error) {
+        console.warn(error);
+      } else if (data) {
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
+      }
+
+      setLoading(false);
+    }
+
+    getProfile();
+  }, [session]);
+
+  async function updateProfile(event: FormEvent, avatarUrl: string) {
+    event.preventDefault();
+
+    setLoading(true);
+    const { user } = session;
+
+    const updates = {
+      id: user.id,
+      username,
+      website,
+      avatarUrl,
+      updated_at: new Date(),
+    };
+
+    let { error } = await supabase.from("profiles").upsert(updates);
+
+    if (error) {
+      alert(error.message);
+    } else {
+      setAvatarUrl(avatarUrl);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <form onSubmit={updateProfile} className="form-widget">
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" value={session.user.email} disabled />
+      </div>
+      <div>
+        <label htmlFor="username">Name</label>
+        <input
+          id="username"
+          type="text"
+          required
+          value={username || ""}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="website">Website</label>
+        <input
+          id="website"
+          type="url"
+          value={website || ""}
+          onChange={(e) => setWebsite(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <button
+          className="button block primary"
+          type="submit"
+          disabled={loading}
+        >
+          {loading ? "Loading ..." : "Update"}
+        </button>
+      </div>
+
+      <div>
+        <button
+          className="button block"
+          type="button"
+          onClick={() => supabase.auth.signOut()}
+        >
+          Sign Out
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ src/components/Auth.tsx
@@ -0,0 +1,49 @@
+import { FormEvent, useState } from "react";
+import { supabase } from "../helpers/supabase";
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleLogin = async (event: FormEvent) => {
+    event.preventDefault();
+
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithOtp({ email });
+
+    if (error) {
+      alert(error.cause || error.message);
+    } else {
+      alert("Check your email for the login link!");
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="row flex flex-center">
+      <div className="col-6 form-widget">
+        <h1 className="header">Supabase + React</h1>
+        <p className="description">
+          Sign in via magic link with your email below
+        </p>
+        <form className="form-widget" onSubmit={handleLogin}>
+          <div>
+            <input
+              className="inputField"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              required={true}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className={"button block"} disabled={loading}>
+              {loading ? <span>Loading</span> : <span>Send magic link</span>}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
