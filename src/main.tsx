e1539f4fc528eba0446418934580175ff0e36ff7
nikolaus@LinuxMint22 <nikolaus@superbase.com>
2023-10-16 10:33:39 +0100

    add new

--- src/main.tsx
+++ src/main.tsx
@@ -52,7 +52,7 @@ async function protectedLoader({ request }: LoaderFunctionArgs) {
   // If the user is not logged in and tries to access a protected route, we redirect
   // them to `/login` with a `from` parameter that allows login to redirect back
   // to this page upon successful authentication
-  if (!(await supabase.auth.getSession()).data.session) {
+  if (!(await supabase.auth.getUser()).data.user) {
     const params = new URLSearchParams();
     params.set("from", new URL(request.url).pathname);
     return redirect("/login?" + params.toString());
