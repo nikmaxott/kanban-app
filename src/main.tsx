0e843e4bdb8ea7d1787114139c47e8575b30d9f6
nikolaus@LinuxMint22 <nikolaus@superbase.com>
2023-10-16 10:28:57 +0100

    Added protectedLoader

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, LoaderFunctionArgs, redirect, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root.js";
 import ErrorPage from "./error-page.js";
 import Auth from "./components/Auth.js";
 import Register from "./routes/register.js";
 import Index from "./routes/index.js";
+import { supabase } from "./helpers/supabase.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    loader: protectedLoader,
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
@@ -35,7 +37,28 @@ const router = createBrowserRouter([
     path: "register",
     element: <Register />,
   },
+  {
+    path: "/logout",
+    async action() {
+      // We signout in a "resource route" that we can hit from a fetcher.Form
+      await supabase.auth.signOut();
+      return redirect("/");
+    },
+  },
 ]);
+
+
+async function protectedLoader({ request }: LoaderFunctionArgs) {
+  // If the user is not logged in and tries to access a protected route, we redirect
+  // them to `/login` with a `from` parameter that allows login to redirect back
+  // to this page upon successful authentication
+  if (!(await supabase.auth.getSession()).data.session) {
+    const params = new URLSearchParams();
+    params.set("from", new URL(request.url).pathname);
+    return redirect("/login?" + params.toString());
+  }
+  return null;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
