a0782335251e3ff1471e1371fa5e1a6242355b34
nikolaus@LinuxMint22 <nikolaus@superbase.com>
2023-10-18 10:24:27 +0100

    register now has basic checks

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,10 +1,27 @@
 import { FormEvent, useState } from "react";
 import { supabase } from "../helpers/supabase";
+import StyledInput from "./Styled/Input";
+import { ErrorMessage, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+const RegisterSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(8, "Password must be 8 characters long")
+    .required("Required"),
+});
 
 export default function RegisterForm() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const initialValues: FormValues = { email: "", password: "" };
 
   async function handleRegister(event: FormEvent) {
     event.preventDefault();
@@ -23,51 +40,53 @@ export default function RegisterForm() {
   }
 
   return (
-    <form className="space-y-4 mt-4" onSubmit={handleRegister}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          className="block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:text-sm sm:leading-6"
-          id="email"
-          type="email"
-          autoComplete="email"
-          placeholder="Your email"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Password</label>
-        <input
-          className="block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:text-sm sm:leading-6"
-          id="password"
-          type="password"
-          placeholder="password"
-          autoComplete="password"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={RegisterSchema}
+      onSubmit={handleRegister}
+    >
+      <Form className="space-y-4 mt-4">
+        <div>
+          <label htmlFor="email">Email</label>
+          <StyledInput
+            id="email"
+            name="email"
+            type="email"
+            autocomplete="email"
+            placeholder="Your email"
+            required={true}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Password</label>
+          <StyledInput
+            id="password"
+            name="password"
+            type="password"
+            placeholder="password"
+            autocomplete="password"
+            required={true}
+          />
+        </div>
 
-      <button
-        className=" w-full justify-center rounded bg-blue-600 h-10 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-        disabled={loading}
-      >
-        {loading ? <span>Loading</span> : <span>Create Account</span>}
-      </button>
-      <p className="text-xs">
-        By clicking "Create account" you agree to our{" "}
-        <a href="tos" className="hover:underline">
-          Terms Of Service
-        </a>{" "}
-        and{" "}
-        <a href="pp" className="hover:underline">
-          Privacy Policy
-        </a>
-        .
-      </p>
-    </form>
+        <button
+          className=" w-full justify-center rounded bg-blue-600 h-10 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          disabled={loading}
+        >
+          {loading ? <span>Loading</span> : <span>Create Account</span>}
+        </button>
+        <p className="text-xs">
+          By clicking "Create account" you agree to our{" "}
+          <a href="tos" className="hover:underline">
+            Terms Of Service
+          </a>{" "}
+          and{" "}
+          <a href="pp" className="hover:underline">
+            Privacy Policy
+          </a>
+          .
+        </p>
+      </Form>
+    </Formik>
   );
 }
