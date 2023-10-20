e0fb9776427b2711cc257732f3d566351520b467
nikolaus@LinuxMint22 <nikolaus@superbase.com>
2023-10-20 14:54:22 +0100

    Forgot password uses Formik

--- src/components/ForgotPasswordForm.tsx
+++ src/components/ForgotPasswordForm.tsx
@@ -1,48 +1,58 @@
-import { FormEvent, useState } from "react";
+import {  useState } from "react";
 import { supabase } from "../helpers/supabase";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import StyledInput from "./Styled/Input";
+
+const ForgotSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+});
 
 export default function ForgotPasswordForm() {
   const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  
-
-  async function handleReset(event: FormEvent) {
-    event.preventDefault();
-
-    setLoading(true);
-    const { data, error } = await supabase.auth.resetPasswordForEmail(email,{
-        redirectTo: 'http://localhost:5173/update-password',
-      });
-
-    console.log(data, error);
-
-    //TODO: Deal with if already has SSO
-
-    setLoading(false);
-  }
+  const [error, setError] = useState<string>();
 
   return (
-    <form className="space-y-4 mt-4" onSubmit={handleReset}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          className="block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:text-sm sm:leading-6"
-          id="email"
-          type="email"
-          autoComplete="email"
-          placeholder="Your email"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <button
-        className=" w-full justify-center rounded bg-blue-600 h-10 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-        disabled={loading}
-      >
-        {loading ? <span>Loading</span> : <span>Reset Password</span>}
-      </button>
-    
-    </form>
+    <Formik
+      initialValues={{ email: "" }}
+      validationSchema={ForgotSchema}
+      onSubmit={async (values) => {
+        setError("");
+        setLoading(true);
+        const { error } = await supabase.auth.resetPasswordForEmail(
+          values.email,
+          {
+            redirectTo: "http://localhost:5173/update-password",
+          },
+        );
+
+        if (error) setError(error.message);
+
+        setLoading(false);
+      }}
+    >
+      <Form className="space-y-4 mt-4">
+        <div>
+          <label htmlFor="email">Email</label>
+          <StyledInput
+            id={"email"}
+            type={"email"}
+            placeholder={"Your email"}
+            autocomplete={"email"}
+            required={true}
+            name={"email"}
+          />
+        </div>
+        <button
+          className=" w-full justify-center rounded bg-blue-600 h-10 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          disabled={loading}
+          type="submit"
+        >
+          {loading ? <span>Loading</span> : <span>Reset Password</span>}
+        </button>
+
+        {error && <span>{error}</span>}
+      </Form>
+    </Formik>
   );
 }
