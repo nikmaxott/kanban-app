9b165f93eb0f34978b109dd440718adcf03c5e0d
Nikolaus-Maxim Zolnhofer <nikolaus14@k-c13.org>
2023-11-19 12:27:40 +0000

    Update package version and add toggle view feature

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,39 @@
+import BoardView from "../components/BoardView";
 import TodoList from "../components/TodoList";
+import { useState } from "react";
+import { supabase } from "../helpers/supabase";
 
 export default function Index() {
+  const [isListView, setIsListView] = useState(true);
+
+  const { data } = await supabase.auth.getSession();
+
+  const toggleView = () => {
+    setIsListView(!isListView);
+  };
+
   return (
     <>
-      <p>INDEX PAGE GOES HERE</p>
-      <TodoList />
+      <div className="flex mt-8">
+        <div className="flex items-center space-x-4">
+          <div className="relative inline-block w-10 mr-2 align-middle select-none">
+            <input
+              type="checkbox"
+              name="toggle"
+              id="toggle"
+              className=" absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+              checked={isListView}
+              onChange={toggleView}
+            />
+            <label
+              htmlFor="toggle"
+              className="block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
+            ></label>
+          </div>
+          Show List View
+        </div>
+      </div>
+      {isListView ? <TodoList /> : <BoardView />}
     </>
   );
 }
